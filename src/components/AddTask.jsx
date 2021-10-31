edaecdd67f849f9839e36a241c28b9135afaf8ba
EduardoM <emaiarezende@gmail.com>
2021-10-31 18:23:11 -0300

    Implementação da barra de adição de tarefas

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./AddTask.css";
+import Button from './Button';
+
+
+const AddTask = () => {
+    return ( 
+        <div className="add-task-container">
+
+            <input className="add-task-input"type="text"></input>
+            
+            
+            <div className="add-task-button-container">
+
+            <Button>Adicionar</Button>
+            </div>
+        </div>
+     );
+}
+ 
+export default AddTask;
---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./Button.css"
+
+const Button = ({children, onclick}) => {
+    return ( 
+        <button onclick={onclick}className="button">
+            {children}
+        </button>
+     );
+}
+ 
+export default Button;
