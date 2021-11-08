a874efc85c38690393ef3ac0ad52e6ff4f6b2042
Eduardo M. Rezende <maiarezendeeduardo@gmail.com>
2021-11-08 11:10:29 -0300

    Implementando funcionalidade de incluir tarefa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React,{useState} from 'react';
-import Tasks from './components/Tasks';
+import {v4 as uuidv4} from 'uuid'
 import "./App.css";
+
+import Tasks from './components/Tasks.jsx';
+
 import AddTask from './components/AddTask';
 
 const App = () => {
@@ -17,11 +20,22 @@ const App = () => {
     },
   ]);
 
+const handleTaskAddition = (taskTitle) =>{
+const newTasks = [
+  ...tasks,
+  {
+  title:taskTitle,
+  id: uuidv4(),
+  completed:false,
+},
+];
 
+setTasks(newTasks);
+};
 
   return( <>
   <div className="container">
-    <AddTask />
+    <AddTask  handleTaskAddition={handleTaskAddition}/>
      <Tasks tasks={tasks}/>
   </div>
   </>
